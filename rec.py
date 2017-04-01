730ee1bfe98d433288016d4e16f5621c9df9e8db
voidrank <voidrank@gmail.com>
2017-04-01 20:45:05 +0800

    first commit

---
+++ rec.py
@@ -0,0 +1,54 @@
+import pyaudio
+import wave
+import pylab as pl
+import numpy as np
+import wave
+import sys
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 176400
+RECORD_SECONDS = 10
+WAVE_OUTPUT_FILENAME = sys.argv[1]
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+print("* recording")
+frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print("* done recording")
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+f = wave.open(WAVE_OUTPUT_FILENAME, "rb")
+params = f.getparams()
+nchannels, sampwidth, framerate, nframes= params[:4]
+
+str_data = f.readframes(nframes)
+f.close()
+wave_data = np.fromstring(str_data, dtype=np.short)
+wave_data.shape = -1, 2
+wave_data = wave_data.T
+time = np.arange(0, nframes) * (1.0 / framerate)
+
+pl.subplot(211) 
+pl.plot(time, wave_data[0])
+pl.subplot(212) 
+pl.plot(time, wave_data[1], c="g")
+pl.xlabel("time (seconds)")
+pl.show()
+
